d04b5343d944e5fba1fd780e911f87aa29f3ba9a
Amjad Rathod <amjad@fatah.co>
2018-06-08 17:21:42 +0530

    change bot layout

--- README.md
+++ README.md
@@ -4,12 +4,13 @@
 
 ### python
 
-  * __language:__ Python 2.7.10
-  * __libraries:__ tweepy, time, multiprocessing
-  * __web framework:__ web.py
+  * __language:__ Python 3.6.1
+  * __libraries:__ tweepy, time, multiprocessing, opencv-python, numpy, workzeug
+  * __web framework:__ flask
   * __style__: PEP 8: https://www.python.org/dev/peps/pep-0008/
-
-  	* Note: for python3 `$ pip3 install git+https://github.com/webpy/webpy#egg=web.py`
+  	* __Note__: for python-2.7.10, download the .zip file from the Release v0.0.4 -
+	this older release uses web.py:
+	* https://github.com/johncoleman83/mini-tweet-bot/releases/tag/v0.0.4
 
 ### cloud:
 
@@ -17,17 +18,20 @@
   * __platform:__ Cloud Foundry, https://www.cloudfoundry.org/
   * __Cloud Foundry command line interface (CLI):__ https://github.com/cloudfoundry/cli
   * __CF python template app:__ https://github.com/IBM-Bluemix/get-started-python
+  * __CF Buildpack:__ https://github.com/heroku/heroku-buildpack-python.git
 
 ### examples:
 
   * __working app:__ https://mtb.mybluemix.net/
   * __blog:__ http://www.davidjohncoleman.com/2017/mini-tweet-bot/
+  * __demo example:__ http://holberton-mtb.mybluemix.net/
+    * __demo sourcecode__: https://github.com/johncoleman83/holberton-mtb
 
 ### twitter:
 
   * __twitter dev tools:__ https://dev.twitter.com/
   * __tweet deck:__ https://tweetdeck.twitter.com/
-  * __twitter (bot) account:__ https://twitter.com/AmjadRathod
+  * __twitter (bot) account:__ https://twitter.com/are_no_one
   * __api limits:__ https://support.twitter.com/articles/160385
   * __best practices:__ https://dev.twitter.com/basics
 
@@ -43,77 +47,31 @@ applications with IBM Bluemix.
 
 ## documentation
 
-For integration with IBM Bluemix, cloudfoundry apps, see the
-[README.md](https://github.com/IBM-bluemix/get-started-python)
+For integration with IBM Bluemix, cloudfoundry apps, see the 
+[README.md](https://github.com/IBM-bluemix/get-started-python) 
 from the below referenced repository.  Or read the blog post referenced above.
 
 ## file List
 
-### cloud foundry
-
-* `./Procfile`: this file contains the initiation script for run the app
-in IBM Bluemix CF
-* `./manifest.yml`: Supports cloud foundry command line interface.
-* `./runtime.txt`: informs cloudfoundry of what version of python to run
-
-### python app files
-
-* `./app.py`
-
-  The main application, used to render web user interface and call all functions
-  of all the features.
-
-* `./censorship.py`
-
-  Module with functions for censoring input text.
-
-* `./mycredentials.py`
-
-  Example of how to use twitter API to integrate with tweepy.  If you attempt
-  to make your own twitter bot, you should rename this file `credentials.py` so
-  that it is imported into `app.py` with the line: `from credentials import *`
-
-* `./profanity.py`, `./suppprt/profanity.txt`, `aldict.py`
-
-  `profanity.py`: set of profaine words; contains 700+ words.  the `.py`
-  file contains a set and is imported into the `censorship.py` module, the
-  `.txt` file is used simply for testing and to more easily share the
-  list.  `aldict.py` contains the ascii-art and L337 translation
-  dictionaries
-
-* `./requirements.txt`
-
-  contains information on the modules necessary to run this app
-
-### `./static/`
-
-  This directory contains all  website support files such as `.css`, `.js`, and
-  fontasesome.io integrations
-
-### `./support/`
-
-  This directory contains old files from Cloud Foundry template, that I did not
-  use, and some other support files explained below.
-
-* `./support/daffodils.txt`
-
-  Example file to show how to input text to automate texting from a file. Each
-  line from the file is tweeted every N seconds.
-
-* `./support/retweet_follow.py`, `./singletweet.py/`, `./tweet_textfile.py`
-
-  These are example files that contain only one function for the event that
-  someone wants to make a twitter bot without integration into the cloud.
-
-### `./templates/`
-
-  This contains all the HTML content as rendered with python.  I used one file
-  as a base layout which contains the same head, header, sidebar, and footer.
-  The main content in the article section changes per GET and POST call.
+* for a complete list of file explanations, please see the file `./FILELIST.md`
 
 ## usage
 
-`$ python app.py`
+* from local machine:
+
+  ```
+  $ python3 app.py
+  ```
+
+* with cloudfoundry CLI:
+
+  ```
+  $ cf login -a api.[my-cloudfoundry].com
+  $ cd [my-app-directory]
+  $ cf push -b https://github.com/heroku/heroku-buildpack-python.git
+  ```
+  Note: use latest heroku python buildpack for most updated python & pip
+  versions
 
 ## build your own bot
 
@@ -130,12 +88,16 @@ in IBM Bluemix CF
    * find the cloud to host the app.  Mini tweet bot is already setup with
    	 Cloud Foundry for IBM Bluemix, but other cloud services will work as well.
 
+### demo screen shot
+<img src="https://github.com/johncoleman83/mini-tweet-bot/blob/master/static/images/screen-shot.png" width="auto" height="400" />
+
 __NOTE:__ The mini tweet bot functions most successfully when hosted on a
 cloud.  However, if you would like to run the app on your own machine, you can
 run it, and it will be loaded on a local host port IP address such as:
 http://0.0.0.0:8080/.  If you do not want the user interface, you should then
 use only the tweet functions, and run them on an as needed basis.  Here is an
-example of how to run a single function from the `singletweet.py` file:
+example of how to run a single function from the `singletweet.py` file in the
+`./support` directory:
 
 ```
 $ cat singletweet.py
@@ -164,4 +126,4 @@ David John Coleman II.	Check out my website [davidjohncoleman.com](http://www.da
 
 ### License
 
-Public Domain, no copyright protection
+MIT License
--- app.py
+++ app.py
@@ -2,20 +2,20 @@
 app python mini tweet bot application
 takes user input and tweets to designated account
 """
-import web
 import tweepy
 import multiprocessing
+import os
+import time
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from werkzeug import secure_filename
 from time import sleep
-from credentials import *
-
-# censoring functions
+from credentials import (consumer_key, consumer_secret, access_token,
+                         access_token_secret)
+# custom imports
 import censorship
+from aldict import ascii_dict, leet_dict
 
-# dictionaries
-from aldict import ascii_dict
-from aldict import leet_dict
-
-# import functions
+# custom variable names from imports
 remove_whitespace = censorship.remove_whitespace
 censor = censorship.censor
 
@@ -24,20 +24,33 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# web.py
-urls = (
-    '/', 'index',
-    '/confirmtweet', 'confirmtweet',
-    '/features', 'features',
-    '/confirmfeature', 'confirmfeature'
-)
-app = web.application(urls, globals())
-render = web.template.render('templates/', base="layout")
+# flask integrations
+app = Flask(__name__)
+port = int(os.getenv('PORT', 8080))
+UPLOAD_FOLDER = './static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = set(['txt', 'png', 'jpg', 'jpeg', 'gif'])
+
+
+# flask support function to verify file import type
+def allowed_file(filename):
+    """checks if file is allowable and safe guards the file"""
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+# adds date to input image file
+def add_date(filename):
+    """makes each file unique by adding date"""
+    extension = filename[-4:]
+    prefix = filename[:-4]
+    thedate = time.strftime("%d-%m-%y--%H:%M:%S")
+    return "{:}{:}{:}".format(prefix, thedate, extension)
+
 
 # translation to ascii or leet
-
-
 def translate(tweetvar, c):
+    """function translates input tweet to leet or ascii art"""
     if c == 'a':
         tweetvar = tweetvar.lower()
         for key in ascii_dict:
@@ -51,8 +64,6 @@ def translate(tweetvar, c):
 
 
 # begin tweet functions here
-
-
 def tweet_text(tweetvar):
     """ tweets text from input variable """
     try:
@@ -60,6 +71,17 @@ def tweet_text(tweetvar):
     except:
         return False
     return True
+
+
+def tweet_image(filename, tweetvar):
+    """ tweets image with status """
+    try:
+        api.update_with_media(filename, tweetvar)
+        return True
+    except tweepy.TweepError as e:
+        print(e.reason)
+        pass
+    return False
 
 
 def retweet_follow(searchterms):
@@ -128,110 +150,128 @@ def auto_tweet_file(filename, seconds):
 def follow_followers():
     """ follow all your followers """
     for follower in tweepy.Cursor(api.followers).items():
+        are_following = follower.__dict__.get('following')
+        request_sent = follower.__dict__.get('follow_request_sent')
         try:
-            follower.follow()
+            if not are_following and not request_sent:
+                follower.follow()
         except tweepy.TweepError as e:
-                print(e.reason)
-                pass
-
-
-# begin web rendering classes here
-class index(object):
-    def GET(self):
-        return render.index(newstring="none")
-
-    def POST(self):
-        form = web.input()
-        try:
-            tweetvar = "%s" % (form.tweet)
-            if censor(tweetvar):
-                if form.translate == "ascii":
-                    tweetvar = translate(tweetvar, 'a')
-                    return render.index(newstring=tweetvar)
-                elif form.translate == "leet":
-                    tweetvar = translate(tweetvar, 'l')
-                    return render.index(newstring=tweetvar)
-                else:
-                    if tweet_text(tweetvar):
-                        return render.confirmtweet(tweetvar=tweetvar)
-            else:
-                return render.confirmtweet(tweetvar="profanity")
-        except:
+            print(e.reason)
             pass
-        return render.confirmtweet(tweetvar="failure")
 
 
-class confirmtweet:
-    def GET(self):
-        return render.confirmtweet(tweetvar="")
+# begin flask template rendering
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    """renders main index page"""
+    if request.method == 'GET':
+        return render_template('index.html', newstring="none")
+    if request.method == 'POST':
+        tweetvar = request.form['tweet']
+        if not censor(tweetvar):
+            return redirect(url_for('confirmtweet', tweetvar="profanity"))
+        if request.form['translate'] == "ascii":
+            tweetvar = translate(tweetvar, 'a')
+        elif request.form['translate'] == "leet":
+            tweetvar = translate(tweetvar, 'l')
+        if request.form['action'] == 'translate':
+            return render_template('index.html', newstring=tweetvar)
+        else:
+            if request.files['file']:
+                file = request.files['file']
+                if allowed_file(file.filename):
+                    filename = secure_filename(file.filename)
+                    filename = os.path.join(UPLOAD_FOLDER, filename)
+                    filename = add_date(filename)
+                    file.save(filename)
+                    if tweet_image(filename, tweetvar):
+                        return redirect(url_for('confirmtweet',
+                                                tweetvar=tweetvar))
+            elif tweet_text(tweetvar):
+                return redirect(url_for('confirmtweet', tweetvar='party time'))
+        return redirect(url_for('confirmtweet', tweetvar='failure'))
 
 
-class features:
-    def GET(self):
-        return render.features()
+@app.route('/confirmtweet/<tweetvar>', methods=['GET'])
+def confirmtweet(tweetvar):
+    """renders confirmation tweet"""
+    return render_template('confirmtweet.html', tweetvar=tweetvar)
 
-    def POST(self):
-        form = web.input(inputfile={})
+
+@app.route('/features', methods=['GET', 'POST'])
+def features():
+    if request.method == 'GET':
+        return render_template('features.html')
+    if request.method == 'POST':
         failcount = 0
-        searchterms = ("%s" % (form.searchterms) if form.searchterms
-                       else "#opensource")
-        seconds = float(form.seconds if form.seconds else 86400)
-        iterations = int(form.iterations if form.iterations else 3)
-        xfollowers = int((form.xfollowers) if form.xfollowers else 3)
+        searchterms = (request.form['searchterms']
+                       if request.form['searchterms'] else '#opensource')
+        seconds = float(request.form['seconds'] if request.form['seconds']
+                        else 86400)
+        iterations = int(request.form['iterations']
+                         if request.form['iterations'] else 3)
+        xfollowers = int(request.form['xfollowers']
+                         if request.form['xfollowers'] else 3)
         if not censor(searchterms):
-            return render.confirmtweet(tweetvar="profanity")
+            return redirect(url_for('confirmtweet', tweetvar='profanity'))
         try:
-            if form.retweet:
+            if request.form['retweet']:
                 if retweet_follow(searchterms) is not True:
                     failcount += 1
         except:
             failcount += 1
-            pass
         try:
-            if form.followfollowers:
+            if request.form['followfollowers']:
                 follow_followers()
         except:
             failcount += 1
-            pass
         try:
-            if form.followx:
+            if request.form['followx']:
                 if follow_x(searchterms, xfollowers) is False:
-                        failcount += 1
+                    failcount += 1
         except:
             failcount += 1
-            pass
         try:
-            if form.autotweet:
-                if 'inputfile' in form:
-                    filepath = form.inputfile.filename.replace('\\', '/')
-                    filename = filepath.split('/')[-1]
-                    fout = open(filename, 'w')
-                    fout.write(form.inputfile.file.read())
-                    fout.close()
+            if request.form['autotweet'] and request.files['file']:
+                file = request.files['file']
+                if allowed_file(file.filename):
+                    filename = secure_filename(file.filename)
+                    filename = os.path.join(UPLOAD_FOLDER, filename)
+                    filename = add_date(filename)
+                    file.save(filename)
                     p = multiprocessing.Process(target=auto_tweet_file,
                                                 args=(filename, seconds))
                     p.start()
+                else:
+                    failcount += 1
         except:
             failcount += 1
-            pass
         try:
-            if form.autoretweet:
+            if request.form['autoretweet']:
                 p = multiprocessing.Process(target=auto_retweet,
                                             args=(searchterms, seconds,
                                                   iterations))
                 p.start()
         except:
             failcount += 1
-            pass
-        if failcount == 5:
-            return render.confirmfeature(status="failure")
+        if failcount >= 5:
+            return redirect(url_for('confirmfeature', status='failure'))
         else:
-            return render.confirmfeature(status="success")
+            return redirect(url_for('confirmfeature', status='success'))
+
+@app.route('/confirmfeature/<status>', methods=['GET'])
+def confirmfeature(status):
+    return render_template('confirmfeature.html', status=status)
 
 
-class confirmfeature:
-    def GET(self):
-        return render.confirmfeature(status="")
+@app.route('/are-no-one', methods=['GET'])
+def easteregg():
+    return render_template('easteregg.html')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=port, debug=True)
