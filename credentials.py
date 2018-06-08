cb1557ea785226138aa606ed4e5dbbb5be1966b6
Amjad Rathod <amjad@fatah.co>
2018-06-08 10:24:10 +0530

    add credentials.py file

---
+++ credentials.py
@@ -0,0 +1,4 @@
+consumer_key = '[yinhgbc7mMszh0vMx4W1bo2ea]'  # (example: '5FmNevRxQriq1')
+consumer_secret = '[YJ2mjUuJJ91ZRr9S4ImP6NqRMCbqRPhnlWuXDZ0OaCjaU5flBL]'  # (example: 'pMKxr4ocPV451')
+access_token = '[1051081958-QxvcA3Yb2riaOgYaYelV76d6aL6GxXpR4TCVoYG]'  # (example: 'ZuPzOYwz2n6o')
+access_token_secret = '[7373WLOfDquRjRDNCd2zWS6dR0salWADURDCJpmhrSK0a]'  # (example: 'TCqaOPc8j1qP')
